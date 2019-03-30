ca6e5d804e914b9a0f0258b0146c9192a9fff560
osycon <fredrichawk@gmail.com>
2019-03-30 20:17:48 +0100

    feat: Added correct fonts to project title, navigation and landing page text.

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -1,5 +1,18 @@
 <template>
-  <section>Hero Section</section>
+  <!-- <section class="hero"></section> -->
+  <section>
+    <v-parallax :src="require('@/assets/hero.jpg')" height="100%">
+      <v-layout column align-center justify-center class="white-text">
+        <h1 class="display-3">
+          <span class="hero-title">Resumes has never been easier</span>
+        </h1>
+        <div class="subheading pl-2 mb-4">
+          Resume builder lets you build your resume from a
+          form or JSON file and personalize it through themes.
+        </div>
+      </v-layout>
+    </v-parallax>
+  </section>
 </template>
 
 <script>
@@ -9,4 +22,7 @@ export default {
 </script>
 
 <style>
+.hero-title {
+  font-family: "Crete Round", serif;
+}
 </style>
--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,14 +1,14 @@
 <template>
   <v-toolbar app>
     <v-toolbar-title class="headline text-uppercase">
-      <span>Resume Builder</span>
+      <span class="logo-title">Resume Builder</span>
     </v-toolbar-title>
     <v-spacer></v-spacer>
     <v-toolbar-items class="hidden-sm-and-down">
-      <v-btn flat text-uppercase to="/">Home</v-btn>
-      <v-btn flat text-uppercase to="/about">About</v-btn>
-      <v-btn flat text-uppercase to="/contact">Contact</v-btn>
-      <v-btn flat text-uppercase to="/resume">Get Started</v-btn>
+      <v-btn flat text-uppercase to="/" class="nav-item">Home</v-btn>
+      <v-btn flat text-uppercase to="/about" class="nav-item">About</v-btn>
+      <v-btn flat text-uppercase to="/contact" class="nav-item">Contact</v-btn>
+      <v-btn flat text-uppercase to="/resume" class="nav-item">Get Started</v-btn>
     </v-toolbar-items>
     <v-toolbar-side-icon class="hidden-md-and-up"></v-toolbar-side-icon>
   </v-toolbar>
@@ -20,5 +20,11 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+.logo-title {
+  font-family: "Gravitas One", cursive;
+}
+.nav-item {
+  font-family: "Crete Round", serif;
+}
 </style>
