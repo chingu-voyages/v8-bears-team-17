4fc4ac1306201b7f33a288e75796307e30dfa080
osycon <fredrichawk@gmail.com>
2019-03-30 20:03:18 +0100

    feat: Added global typography.

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import Vue from 'vue';
-import './plugins/vuetify';
 import App from './App.vue';
-import router from './router';
+import './plugins/vuetify';
 import './registerServiceWorker';
+import router from './router';
+import './stylus/main.styl';
 
 Vue.config.productionTip = false;
 
