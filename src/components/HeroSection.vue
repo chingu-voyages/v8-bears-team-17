f8d73e49f6559c97d9fe9113dd655de7972c4d8d
osycon <fredrichawk@gmail.com>
2019-03-31 19:42:59 +0200

    feat: Added colors to fonts.

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -1,12 +1,18 @@
 <template>
-  <!-- <section class="hero"></section> -->
   <section>
-    <v-parallax :src="require('@/assets/hero.jpg')" height="100%">
-      <v-layout column align-center justify-center class="white-text">
-        <h1 class="display-3">
+    <v-parallax :src="require('@/assets/hero.jpg')" class="hero-image">
+      <v-layout column align-center justify-center>
+        <h1 class="display-1 hidden-md-and-up">
           <span class="hero-title">Resumes has never been easier</span>
         </h1>
-        <div class="subheading pl-2 mb-4">
+        <h1 class="display-3 hidden-sm-and-down">
+          <span class="hero-title">Resumes has never been easier</span>
+        </h1>
+        <div class="subheading pl-2 mb-4 hidden-md-and-up">
+          Resume builder lets you build your resume from a
+          form or JSON file and personalize it through themes.
+        </div>
+        <div class="headline pl-2 mb-4 hidden-sm-and-down">
           Resume builder lets you build your resume from a
           form or JSON file and personalize it through themes.
         </div>
@@ -22,7 +28,18 @@ export default {
 </script>
 
 <style>
+.hero-image {
+  height: 100vh;
+}
+.hero-image img {
+  opacity: 0.25 !important;
+}
 .hero-title {
   font-family: "Crete Round", serif;
+  color: #3c5779;
+}
+.subheading,
+.headline {
+  color: #24a4b3;
 }
 </style>
--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,16 +1,27 @@
 <template>
   <v-toolbar app>
-    <v-toolbar-title class="headline text-uppercase">
+    <v-toolbar-title class="headline text-uppercase hidden-sm-and-down">
+      <span class="logo-title">Resume Builder</span>
+    </v-toolbar-title>
+    <v-toolbar-title class="title text-uppercase hidden-md-and-up">
       <span class="logo-title">Resume Builder</span>
     </v-toolbar-title>
     <v-spacer></v-spacer>
     <v-toolbar-items class="hidden-sm-and-down">
-      <v-btn flat text-uppercase to="/" class="nav-item">Home</v-btn>
-      <v-btn flat text-uppercase to="/about" class="nav-item">About</v-btn>
-      <v-btn flat text-uppercase to="/contact" class="nav-item">Contact</v-btn>
-      <v-btn flat text-uppercase to="/resume" class="nav-item">Get Started</v-btn>
+      <v-btn flat text-uppercase to="/" class="nav-item">
+        <span>Home</span>
+      </v-btn>
+      <v-btn flat text-uppercase to="/about" class="nav-item">
+        <span>About</span>
+      </v-btn>
+      <v-btn flat text-uppercase to="/contact" class="nav-item">
+        <span>Contact</span>
+      </v-btn>
+      <v-btn flat text-uppercase to="/resume" class="nav-item">
+        <span>Get Started</span>
+      </v-btn>
     </v-toolbar-items>
-    <v-toolbar-side-icon class="hidden-md-and-up"></v-toolbar-side-icon>
+    <v-toolbar-side-icon class="hidden-md-and-up menu-icon"></v-toolbar-side-icon>
   </v-toolbar>
 </template>
 
@@ -23,8 +34,16 @@ export default {
 <style scoped>
 .logo-title {
   font-family: "Gravitas One", cursive;
+  color: #24a4b3;
 }
 .nav-item {
   font-family: "Crete Round", serif;
+}
+.nav-item span {
+  color: #24a4b3;
+}
+
+.menu-icon i.v-icon {
+  color: #24a4b3;
 }
 </style>
