c5dd58d56d098b20529009cd859aa6db3b3cf93d
osycon <fredrichawk@gmail.com>
2019-04-04 07:14:31 +0200

    fix: Adjusted styles to work on both mobile and desktop.

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -1,9 +1,9 @@
 <template>
-  <section>
-    <v-parallax :src="require('@/assets/hero.jpg')" class="hero-image">
-      <v-layout column align-center justify-center>
+  <section class="hero-section">
+    <v-layout column align-center justify-center class="pa-3">
         <h1 class="display-1 mb-5 hero-title hidden-md-and-up">Resumes has never been easier</h1>
-        <h1 class="display-3 hero-title hidden-sm-and-down">Resumes has never been easier</h1>
+        <h1 class="display-3 mb-5 hero-title hidden-sm-and-down">Resumes has never been easier</h1>
+      <article >
         <div class="subheading mb-4 hidden-md-and-up">
           Resume builder lets you build your resume from a
           form or JSON file and personalize it through themes.
@@ -13,8 +13,8 @@
           form or JSON file and personalize it through themes.
         </div>
         <v-btn color="success get-started">Get started</v-btn>
-      </v-layout>
-    </v-parallax>
+      </article>
+    </v-layout>
   </section>
 </template>
 
@@ -25,16 +25,27 @@ export default {
 </script>
 
 <style>
-.hero-image {
-  height: calc(100vh + 56px) !important;
+.hero-section {
+  height: 100vh;
+  background-image: linear-gradient(
+      rgba(250, 250, 250, 0.75),
+      rgba(250, 250, 250, 0.75)
+    ),
+    url("../assets/hero.jpg");
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
 }
-.hero-image img {
-  opacity: 0.25 !important;
+.layout {
+  height: 100%;
+}
+
+article {
+  max-width: 30rem;
 }
 h1.hero-title {
   font-family: "Crete Round", serif;
   color: #3c5779;
-  margin-top: -40%;
 }
 .subheading,
 .headline {
