06fc75318dbe6c471d3f9192fe207364ad530e49
osycon <fredrichawk@gmail.com>
2019-04-04 06:51:00 +0200

    fix: Disabled showing the navigation drawer on desktop.

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -24,7 +24,7 @@
         </v-btn>
       </v-toolbar-items>
     </v-toolbar>
-    <v-navigation-drawer app v-model="drawer">
+    <v-navigation-drawer app clipped disable-resize-watcher v-model="drawer">
       <v-list>
         <v-list-tile v-for="link in links" :key="link.text" router :to="link.route">
           <v-list-tile-action>
