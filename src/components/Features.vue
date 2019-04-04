5ebe6b268956f5b9c651ecb8eec642610997479e
osycon <fredrichawk@gmail.com>
2019-04-04 07:32:39 +0200

    feat: Styled feature section for desktop.

--- src/components/Features.vue
+++ src/components/Features.vue
@@ -1,26 +1,26 @@
 <template>
   <v-container>
-    <v-layout row>
-      <v-img :src="require('@/assets/img1.jpg')" class="mr-4"></v-img>
-      <v-img :src="require('@/assets/img2.jpg')"></v-img>
+    <v-layout row justify-space-between>
+      <v-img max-width="500" :src="require('@/assets/img1.jpg')" class="mr-3"></v-img>
+      <v-img max-width="500" :src="require('@/assets/img2.jpg')" class="ml-3"></v-img>
     </v-layout>
-    <v-layout row>
-      <div class="subheading mb-4 mt-4 hidden-md-and-up feature-text">
+    <v-layout row class="hidden-md-and-up">
+      <div class="subheading mb-4 mt-4 feature-text">
         <p>
           Choose a style that suits
           <strong>you</strong>.
         </p>
       </div>
     </v-layout>
-    <v-layout row>
-      <div class="headline pl-2 mb-4 hidden-sm-and-down">
+    <v-layout row align-center justify-center class="hidden-sm-and-down">
+      <div class="display-1 mb-5 mt-5 feature-text-desktop">
         Choose a style that suits
         <strong>you</strong>.
       </div>
     </v-layout>
-    <v-layout row>
-      <v-img :src="require('@/assets/img3.jpg')" class="mr-4"></v-img>
-      <v-img :src="require('@/assets/img4.jpg')"></v-img>
+    <v-layout row justify-space-between>
+      <v-img max-width="500" :src="require('@/assets/img3.jpg')" class="mr-3"></v-img>
+      <v-img  max-width="500"  :src="require('@/assets/img4.jpg')"  class="ml-3"></v-img>
     </v-layout>
   </v-container>
 </template>
@@ -33,11 +33,14 @@ export default {
 
 <style>
 .subheading,
-.headline {
+.display-1 {
   color: #24a4b3;
 }
 .feature-text {
   width: 100%;
   text-align: center;
+}
+.feature-text-desktop {
+  max-width: 20rem;
 }
 </style>
