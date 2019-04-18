aab5ead2d75c297bdfe3653bc699ca4fe75b1bd7
Wryhder <jemimahsummers@yahoo.com>
2019-04-18 14:47:50 +0100

    Create initial markup for a basic theme structure

---
+++ src/themes/basic.vue
@@ -0,0 +1,89 @@
+<template>
+    <div class="basic-theme">
+        <section class="personal-data">
+            <img src="" alt="Picture">
+            <h1 class="names"></h1>
+            <p class="location"></p>
+
+            <section class="contact-details">
+                <p class="phone-number"></p>
+                <p class="email-address"></p>
+                <p class="website-address"></p>
+            </section>
+
+            <section class="social-profiles">
+                <p class="twitter"></p>
+                <p class="github"></p>
+                <p class="chingu"></p>
+            </section>
+        </section>
+
+        <section class="work-volunteer-experience">
+            <p class="position"></p>
+            <p class="duration"></p>
+            <section class="company-details">
+                <p class="company-name"></p>
+                <p class="company-website"></p>
+            </section>
+            <p class="summary-of-responsibilities"></p>
+            <ul class="job-highlights">
+                <li class="job-hightlight"></li>
+            </ul>
+        </section>
+
+        <section class="education">
+            <p class="institution"></p>
+            <p class="area"></p>
+            <p class="study-type"></p>
+            <p class="gpa"></p>
+            <p class="duration"></p>
+
+            <ul class="courses">
+                <li class="course"></li>
+            </ul>
+        </section>
+
+        <section class="awards">
+            <p class="title"></p>
+            <p class="date"></p>
+            <p class="awarder"></p>
+            <p class="summary"></p>
+        </section>
+
+        <section class="publications">
+            <p class="name"></p>
+            <section class="publisher-details">
+                <p class="publisher-name"></p>
+                <p class="publisher-website"></p>
+            </section>
+            <p class="release-date"></p>
+            <p class="summary"></p>
+        </section>
+
+        <section class="skills">
+            <p class="skill"></p>
+            <p class="skill-level"></p>
+            <ul class="keywords">
+                <li class="keyword"></li>
+            </ul>
+        </section>
+
+        <section class="languages">
+            <p class="language"></p>
+            <p class="fluency"></p>
+        </section>
+
+        <section class="interests">
+            <p class="name-of-interest">
+                <ul class="keywords">
+                    <li class="keyword"></li>
+                </ul>
+            </p>
+        </section>
+
+        <section class="references">
+            <p class="name-of-reference"></p>
+            <p class="contact-of-reference"></p>
+        </section>
+    </div>
+</template>
