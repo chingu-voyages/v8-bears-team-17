9f0c8e2545f58d30667e2941a14ca1bfee5792c4
osycon <fredrichawk@gmail.com>
2019-04-25 18:53:46 +0200

    feat: Added list displaying values.

---
+++ src/components/UserinfoCard.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-flex xs12 sm6 md4>
+    <v-card class="ma-2 pa-2">
+      <v-card-title class="headline">{{ cardTitle }}</v-card-title>
+      <div class="teal lighten-5 pa-2 ma-1">
+        <v-card-text v-for="(value, key, index) in info"
+        :key="index">
+          <p class="label-text text-uppercase"
+          >{{ capitalizeFirstLetter(key) }}</p>
+          <p class="body-2">{{ value }}</p>
+          <v-divider></v-divider>
+        </v-card-text>
+        <v-spacer></v-spacer>
+      </div>
+    </v-card>
+  </v-flex>
+</template>
+
+<script>
+export default {
+  props: {
+    info: {
+      type: Object,
+    },
+    cardTitle: {
+      type: String,
+    },
+  },
+  methods: {
+    capitalizeFirstLetter(str) {
+      return str.charAt(0).toUpperCase() + str.slice(1);
+    },
+  },
+};
+</script>
+
+<style>
+.card {
+  align-items: stretch;
+}
+.label-text {
+  font-size: 10px;
+}
+</style>
--- src/views/Resume.vue
+++ src/views/Resume.vue
@@ -4,7 +4,9 @@
     <v-flex>
       <JSONUploader v-model="fileContent" ButtonText="Upload json file"></JSONUploader>
     </v-flex>
-
+    <v-flex>
+      <Userinfo :userdata="mainUserData"></Userinfo>
+    </v-flex>
     <v-flex>
       <UserInputForm v-bind:userdata="mainUserData"></UserInputForm>
     </v-flex>
@@ -20,30 +22,32 @@
 
 <script>
 // @ is an alias to /src
-import JSONUploader from "@/components/JSONUploader.vue";
-import nullData from "@/assets/nullData.json";
-import UserInputForm from "@/components/UserInputForm.vue";
-import DownloadJSONFile from "@/components/DownloadJSONFile.vue";
+import JSONUploader from '@/components/JSONUploader.vue';
+import nullData from '@/assets/nullData.json';
+import UserInputForm from '@/components/UserInputForm.vue';
+import DownloadJSONFile from '@/components/DownloadJSONFile.vue';
+import Userinfo from '@/components/Userinfo.vue';
 
 export default {
-  name: "resume",
+  name: 'resume',
   components: {
     JSONUploader,
     UserInputForm,
-    DownloadJSONFile
+    DownloadJSONFile,
+    Userinfo,
   },
   data() {
     return {
       fileContent: null,
       mainUserData: nullData,
-      keys: Array
+      keys: Array,
     };
   },
   methods: {
     testFun() {
-      this.mainUserData.personalData.name = "Luca Gessi";
+      this.mainUserData.personalData.name = 'Luca Gessi';
       this.keys = Object.keys(this.mainUserData);
-    }
+    },
   },
   watch: {
     // whenever fileContent changes, this function will run
@@ -52,7 +56,7 @@ export default {
       console.log(`name basics: ${typeof val.basics}`);
       console.log(`name type: ${typeof val.basics.name}`);
       this.mainUserData = this.fileContent;
-    }
-  }
+    },
+  },
 };
 </script>
