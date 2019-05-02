3acf4c891906c7364d1fc34b1ab872e526536433
lakrids-mint <mettepernille@hotmail.com>
2019-05-02 22:28:31 +0200

    footer display none test

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,16 +1,16 @@
 <template>
-  <v-footer height="auto">
+  <v-footer height="auto" class="no-print">
     <v-layout justify-space-around row wrap class="footer-color">
       <v-card-title>
         <strong>Make your resume shine!</strong>
-        <v-divider class="mx-3" inset vertical></v-divider>
-        Email us:
+        <v-divider class="mx-3" inset vertical></v-divider>Email us:
         <v-btn icon>
           <v-icon color="white" size="24px">email</v-icon>
         </v-btn>
       </v-card-title>
       <v-card-actions justify-center>
-        &copy;2019 - <strong>17 BEARS</strong>
+        &copy;2019 -
+        <strong>17 BEARS</strong>
       </v-card-actions>
     </v-layout>
   </v-footer>
@@ -18,9 +18,8 @@
 
 <script>
 export default {
-  name: 'SiteFooter',
-  data: () => ({
-  }),
+  name: "SiteFooter",
+  data: () => ({})
 };
 </script>
 
@@ -28,5 +27,10 @@ export default {
 .footer-color {
   background: #24a4b3;
   color: white;
+}
+@media print {
+  .no-print {
+    display: none;
+  }
 }
 </style>
