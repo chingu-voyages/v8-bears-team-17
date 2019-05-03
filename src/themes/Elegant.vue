a9123d6b35c4a59e137f6e620267d26d73da90fe
lakrids-mint <mettepernille@hotmail.com>
2019-05-03 17:33:40 +0200

    testing pdf export

--- src/themes/Elegant.vue
+++ src/themes/Elegant.vue
@@ -1,187 +1,197 @@
 <template>
-  <div class="elegant container">
-    <!-- left-col------------------------------------------------------------------------------------- -->
-    <div id="left-col">
-      <section class="img">
-        <!-- Profile pic, name and job title -->
-        <img
-          v-if="previewData.basics.picture"
-          :src="previewData.basics.picture"
-          alt="profile picture"
-        >
-      </section>
-
-      <!-- Contact info - let's use font-awesome(will implement later) -->
-      <h1>{{previewData.basics.name}}</h1>
-      <h2>{{previewData.basics.label}}</h2>
-      <section class="contact-info">
-        <h2>Contact information</h2>
-        <ul>
-          <li>
-            <i class="fas fa-at"></i>
-            {{previewData.basics.email}}
-          </li>
-          <li>
-            <i class="fas fa-phone"></i>
-            {{previewData.basics.phone}}
-          </li>
-          <li>
-            <i class="fas fa-globe"></i>
-            <a :href="previewData.basics.website">{{previewData.basics.website}}</a>
-          </li>
-        </ul>
-      </section>
-      <!-- Social network profiles /personally I think this belongs with contact info-->
-      <section class="social-profiles">
-        <ul v-for="profile in previewData.basics.profiles" :key="profile">
-          <li>
-            <!-- TODO: make dynamic -->
-            <i class="fab fa-twitter"></i>
-            <a :href="profile.url">{{profile.url}}</a>
-          </li>
-        </ul>
-      </section>
-      <!-- Location data -->
-      <section class="location">
-        <address>
-          <p>{{previewData.basics.location.address}}</p>
-          <p>{{previewData.basics.location.postalCode}}</p>
-          <p>{{previewData.basics.location.city}}</p>
-          <p>{{previewData.basics.location.region}}</p>
-          <p>{{previewData.basics.location.countryCode}}</p>
-        </address>
-      </section>
-
-      <!-- Languages -->
-      <section class="languages">
-        <h2>Language skills</h2>
-        <div v-for="language in previewData.languages" :key="language">
-          <p>{{language.language}}</p>
-          <p>{{language.fluency}}</p>
-        </div>
-      </section>
-
-      <!-- Awards -->
-      <section class="awards">
-        <h2>Awards</h2>
-        <div v-for="award in previewData.awards" :key="award">
-          <p>{{award.title}}</p>
-          <p>{{award.date}}</p>
-          <p>{{award.awarder}}</p>
-          <p>{{award.summary}}</p>
-        </div>
-      </section>
-      <!-- References -->
-      <section class="references">
-        <h2>References</h2>
-        <div v-for="reference in previewData.references" :key="reference">
-          <p>{{reference.name}} | {{reference.reference}}</p>
-        </div>
-      </section>
-      <!-- Interests -->
-      <section class="interests">
-        <h2>Interests</h2>
-        <div v-for="interest in previewData.interests" :key="interest">
-          <p>{{interest.name}}</p>
-          <ul>
-            <li v-for="keyword in interest.keywords">{{keyword.keyword}}</li>
-          </ul>
-        </div>
-      </section>
+  <main>
+    <div>
+      <v-btn @click="exportPDF">Export as PDF</v-btn>
     </div>
-    <!-- End left-col -->
+    <div id="preview" class="elegant container">
+      <!-- left-col------------------------------------------------------------------------------------- -->
+      <div id="left-col">
+        <section class="img">
+          <!-- Profile pic, name and job title -->
+          <img
+            v-if="previewData.basics.picture"
+            :src="previewData.basics.picture"
+            alt="profile picture"
+          >
+        </section>
 
-    <!-- right-col------------------------------------------------------------------------- -->
-
-    <div class="right-col">
-      <!-- keeping summary in seperate section for layout flexibility -->
-      <section class="summary">
-        <h2>Summary</h2>
-        <p>{{previewData.basics.summary}}</p>
-      </section>
-      <!-- Skills -->
-      <section class="skills">
-        <h2>
-          <i class="fas fa-tools"></i>Skills
-        </h2>
-        <div v-for="skill in previewData.skills" :key="skill">
-          <span>
-            <p>
-              {{skill.name}} |
-              {{skill.level}}
-            </p>
-          </span>
-
-          <!--skill keywords  -->
-
-          <v-chip outline class="pink" v-for="keyword in skill.keywords">{{keyword.keyword}}</v-chip>
-        </div>
-      </section>
-      <!-- Work experience -------------------------------------------------------------------------------->
-      <section class="work">
-        <h2>
-          <i class="fas fa-briefcase"></i>Work experience
-        </h2>
-        <div v-for="work in previewData.work" :key="work">
-          <span class="subheading">{{work.position}} | {{work.company}}</span>
-          <span class="date">{{work.startDate}} - {{work.endDate}}</span>
-
-          <p>{{work.summary}}</p>
-          <!--work highlights  ----------------------------------------------------------------------->
+        <!-- Contact info - let's use font-awesome(will implement later) -->
+        <h1>{{previewData.basics.name}}</h1>
+        <h2>{{previewData.basics.label}}</h2>
+        <section class="contact-info">
+          <h2>Contact information</h2>
           <ul>
-            <li v-for="highlight in work.highlights" :key="highlight">{{highlight.highlight}}</li>
+            <li>
+              <i class="fas fa-at"></i>
+              {{previewData.basics.email}}
+            </li>
+            <li>
+              <i class="fas fa-phone"></i>
+              {{previewData.basics.phone}}
+            </li>
+            <li>
+              <i class="fas fa-globe"></i>
+              <a :href="previewData.basics.website">{{previewData.basics.website}}</a>
+            </li>
           </ul>
-        </div>
-      </section>
-      <!-- Education -------------------------------------------------------------------------------------------->
-      <section class="education">
-        <h2>
-          <i class="fas fa-graduation-cap"></i>Education
-        </h2>
-        <div v-for="education in previewData.education" :key="education">
-          <span class="subheading">{{education.studyType}} in {{education.area}}</span>
-          <p>{{education.institution}}</p>
+        </section>
+        <!-- Social network profiles /personally I think this belongs with contact info-->
+        <section class="social-profiles">
+          <ul v-for="profile in previewData.basics.profiles" :key="profile">
+            <li>
+              <!-- TODO: make dynamic -->
+              <i class="fab fa-twitter"></i>
+              <a :href="profile.url">{{profile.url}}</a>
+            </li>
+          </ul>
+        </section>
+        <!-- Location data -->
+        <section class="location">
+          <address>
+            <p>{{previewData.basics.location.address}}</p>
+            <p>{{previewData.basics.location.postalCode}}</p>
+            <p>{{previewData.basics.location.city}}</p>
+            <p>{{previewData.basics.location.region}}</p>
+            <p>{{previewData.basics.location.countryCode}}</p>
+          </address>
+        </section>
 
-          <span class="date">{{education.startDate}}- {{education.endDate}}</span>
-          <!--education courses  -->
-          <h3>Courses</h3>
-          <ul>
-            <li v-for="course in education.courses" :key="course">{{course.course}}</li>
-          </ul>
-        </div>
-      </section>
-      <!-- Volunteer experience -->
-      <section class="experience">
-        <h2>
-          <i class="fas fa-hands-helping"></i>Volunteer experience
-        </h2>
-        <div v-for="experience in previewData.volunteer" :key="experience">
-          <span class="subheading">{{experience.position}} | {{experience.company}}</span>
-          <span class="date">{{experience.startDate}} - {{experience.endDate}}</span>
-          <p>{{experience.summary}}</p>
-          <!--volunteer highlights  -->
-          <ul>
-            <li v-for="highlight in experience.highlights" :key="highlight">{{highlight.highlight}}</li>
-          </ul>
-        </div>
-      </section>
+        <!-- Languages -->
+        <section class="languages">
+          <h2>Language skills</h2>
+          <div v-for="language in previewData.languages" :key="language">
+            <p>{{language.language}}</p>
+            <p>{{language.fluency}}</p>
+          </div>
+        </section>
 
-      <!-- Publications -->
-      <section class="publications">
-        <h2>
-          <i class="far fa-newspaper"></i>Publications
-        </h2>
-        <div v-for="publication in previewData.publications" :key="publication">
-          <span class="subheading">{{publication.name}} | {{publication.publisher}}</span>
-          <span class="date">{{publication.releaseDate}} | {{publication.website}}</span>
-          <p>{{publication.summary}}</p>
-        </div>
-      </section>
+        <!-- Awards -->
+        <section class="awards">
+          <h2>Awards</h2>
+          <div v-for="award in previewData.awards" :key="award">
+            <p>{{award.title}}</p>
+            <p>{{award.date}}</p>
+            <p>{{award.awarder}}</p>
+            <p>{{award.summary}}</p>
+          </div>
+        </section>
+        <!-- References -->
+        <section class="references">
+          <h2>References</h2>
+          <div v-for="reference in previewData.references" :key="reference">
+            <p>{{reference.name}} | {{reference.reference}}</p>
+          </div>
+        </section>
+        <!-- Interests -->
+        <section class="interests">
+          <h2>Interests</h2>
+          <div v-for="interest in previewData.interests" :key="interest">
+            <p>{{interest.name}}</p>
+            <ul>
+              <li v-for="keyword in interest.keywords">{{keyword.keyword}}</li>
+            </ul>
+          </div>
+        </section>
+      </div>
+      <!-- End left-col -->
+
+      <!-- right-col------------------------------------------------------------------------- -->
+
+      <div class="right-col">
+        <!-- keeping summary in seperate section for layout flexibility -->
+        <section class="summary">
+          <h2>Summary</h2>
+          <p>{{previewData.basics.summary}}</p>
+        </section>
+        <!-- Skills -->
+        <section class="skills">
+          <h2>
+            <i class="fas fa-tools"></i>Skills
+          </h2>
+          <div v-for="skill in previewData.skills" :key="skill">
+            <span>
+              <p>
+                {{skill.name}} |
+                {{skill.level}}
+              </p>
+            </span>
+
+            <!--skill keywords  -->
+
+            <v-chip outline class="pink" v-for="keyword in skill.keywords">{{keyword.keyword}}</v-chip>
+          </div>
+        </section>
+        <!-- Work experience -------------------------------------------------------------------------------->
+        <section class="work">
+          <h2>
+            <i class="fas fa-briefcase"></i>Work experience
+          </h2>
+          <div v-for="work in previewData.work" :key="work">
+            <span class="subheading">{{work.position}} | {{work.company}}</span>
+            <span class="date">{{work.startDate}} - {{work.endDate}}</span>
+
+            <p>{{work.summary}}</p>
+            <!--work highlights  ----------------------------------------------------------------------->
+            <ul>
+              <li v-for="highlight in work.highlights" :key="highlight">{{highlight.highlight}}</li>
+            </ul>
+          </div>
+        </section>
+        <!-- Education -------------------------------------------------------------------------------------------->
+        <section class="education">
+          <h2>
+            <i class="fas fa-graduation-cap"></i>Education
+          </h2>
+          <div v-for="education in previewData.education" :key="education">
+            <span class="subheading">{{education.studyType}} in {{education.area}}</span>
+            <p>{{education.institution}}</p>
+
+            <span class="date">{{education.startDate}}- {{education.endDate}}</span>
+            <!--education courses  -->
+            <h3>Courses</h3>
+            <ul>
+              <li v-for="course in education.courses" :key="course">{{course.course}}</li>
+            </ul>
+          </div>
+        </section>
+        <!-- Volunteer experience -->
+        <section class="experience">
+          <h2>
+            <i class="fas fa-hands-helping"></i>Volunteer experience
+          </h2>
+          <div v-for="experience in previewData.volunteer" :key="experience">
+            <span class="subheading">{{experience.position}} | {{experience.company}}</span>
+            <span class="date">{{experience.startDate}} - {{experience.endDate}}</span>
+            <p>{{experience.summary}}</p>
+            <!--volunteer highlights  -->
+            <ul>
+              <li
+                v-for="highlight in experience.highlights"
+                :key="highlight"
+              >{{highlight.highlight}}</li>
+            </ul>
+          </div>
+        </section>
+
+        <!-- Publications -->
+        <section class="publications">
+          <h2>
+            <i class="far fa-newspaper"></i>Publications
+          </h2>
+          <div v-for="publication in previewData.publications" :key="publication">
+            <span class="subheading">{{publication.name}} | {{publication.publisher}}</span>
+            <span class="date">{{publication.releaseDate}} | {{publication.website}}</span>
+            <p>{{publication.summary}}</p>
+          </div>
+        </section>
+      </div>
     </div>
-  </div>
+  </main>
 </template>
 
 <script>
+import jsPDF from "jspdf";
+
 export default {
   props: {
     previewData: {
@@ -189,21 +199,22 @@ export default {
       required: true
     }
   },
-  //TODO: format date to month/year
-  filters: {
-    dateFormat: function(value) {
-      if (!value) return "";
-      value = value.toString();
-      let dateFormatted = "";
-      console.log(value);
-    }
-  },
-  created() {},
   computed: {
     test() {
       const dData = JSON.stringify(this.previewData, null, 2);
       console.log(dData);
       return dData;
+    }
+  },
+  methods: {
+    exportPDF() {
+      // eslint-disable-next-line
+      const doc = new jsPDF();
+      const source = window.document.getElementById("preview");
+      console.log("Source", source);
+      doc.fromHTML(source, 10, 10, {}, () => {
+        doc.save("resume.pdf");
+      });
     }
   }
 };
@@ -330,3 +341,5 @@ i {
   }
 }
 </style>
+
+
